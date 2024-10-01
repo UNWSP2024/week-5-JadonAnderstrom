840830de93b7d05f70da861081ea5288b5e5c7f0
JadonAnderstrom <jjanderstrom@students.unwsp.edu>
2024-10-01 18:33:12 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,11 +1,33 @@
 # Program #2: Math Quiz
-# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
+# Write a program that gives simple math quizzes.  The program should display two random numbers to be added
 
-#     247
+import random
 
-# + 129
+def generate_quiz():
 
-# ------
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    answer = num1 + num2
+    return num1, num2, answer
+
+def quiz_question(num1, num2):
+    print(f'  {num1}\n+ {num2}\n====')
+    user_answer = int(input('Enter your answer: '))
+    return user_answer
+
+def check_answer(user_answer, correct_answer):
+    if user_answer == correct_answer:
+        print('Congratulations! Your answer is correct.')
+    else:
+        print(f'Incorrect. The correct answer is {correct_answer}.')
+
+def main():
+    num1, num2, answer = generate_quiz()
+    user_answer = quiz_question(num1, num2)
+    check_answer(user_answer, answer)
+
+if __name__ == "__main__":
+    main()
 
 # The program should allow the student to enter the answer.  
 # If the answer is correct, a message of congratulations should be displayed.  
